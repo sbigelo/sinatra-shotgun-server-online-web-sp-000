2d947295480ed4df60feb7e41a9dfdec3356fe00
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-07 22:42:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,9 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do
-    "Welcome to your app!!!! "
-  end
+
 
   get '/' do
    "Welcome to your app!!!! I BUILT THIS!"
